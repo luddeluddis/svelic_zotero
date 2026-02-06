9994d9a55cefe4701abebba744fbf76304012827
Johan Quester <52780610+luddeluddis@users.noreply.github.com>
2026-02-06 12:41:49 +0100

    Add Drug information detection. Detection now primarily using title

--- translators/UpToDate_article.js
+++ translators/UpToDate_article.js
@@ -9,94 +9,113 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2023-08-18 10:18:43"
+	"lastUpdated": "2026-02-06 11:31:41"
 }
 
-function detectWeb(doc, url)	{
-	if (ZU.xpathText(doc, '//*[@id="topicTitle"]')){
-		return 'bookSection';
-	} else if (url.includes('drug-interactions')){
-		// if visiting interaction database
+function detectWeb(doc, url) {
+	// 1. Check for Drug Interactions tool
+	if (url.includes('drug-interactions')) {
 		return 'journalArticle';
+	}
+
+	// 2. Check for Standard Articles or Drug Info Pages
+	if (url.includes('/contents/')) {
+		return 'bookSection';
+	}
+
+	// 3. Fallback
+	if (doc.title && doc.title.includes("UpToDate")) {
+		return "bookSection";
 	}
 }
 
 function doWeb(doc, url) {
+	var type = detectWeb(doc, url);
 
-	// if interaction page loaded, create interaction reference
-	if (detectWeb(doc, url) == "journalArticle") {
-		
+	if (type == "journalArticle") {
 		var item = new Zotero.Item("webpage");
 		item.title = "Lexicomp® Drug Interactions";
-		item.place  = "Waltham, MA, USA";
-		item.publisher = "UpToDate, Inc."
+		item.place = "Waltham, MA, USA";
+		item.publisher = "UpToDate, Inc.";
 		item.url = "https://www.uptodate.com/drug-interactions";
-		item.date = "";
+		item.complete();
 	} 
-
-	//
-	if  (detectWeb(doc, url) == "bookSection"){
-		// create new reference (item)
+	
+	else if (type == "bookSection") {
 		var item = new Zotero.Item("bookSection");
-		// get full string of all contributers
-		var contrib = ZU.xpathText(doc,'//*[@id="topicContributors"]');
-		// get name of section editor to identify when author list has ended
-		var sect_edit = contrib.match('Section Editors?:[^,]*')[0].match(':.*')[0].substring(1);
-		// convert section-editor to regexp item
-		var sect_edit_test = new RegExp(sect_edit)
-		// find all authors while looping through each contributer and stop when we reach the section editor
-		let authors = new Array();
-		for (i = 1; i <= 5; i++){
-			reg_str = ('//*[@id="topicContributors"]/dd[').concat(i,']/a')
-			var suggest = ZU.xpathText(doc,reg_str).match('[^,]*')[0];
-			if (sect_edit_test.test(suggest)) { 
-				break; 
-			}
-			authors[i-1] = suggest;
-		}
-		// get name of editor (out of laziness we ignore any case of >1 editors)
-		var deputy_editor = contrib.match('Deputy Editors?:[^,]*')[0].match(':.*')[0].substring(1);
-		// get title 
-		var title = ZU.xpathText(doc,'//*[@id="topicTitle"]');
-		// get date of last update
-		var updated = ZU.xpathText(doc, '//div[@class="litReviewSingleLine"]/span[contains(text(), "This topic last updated:")]/following-sibling::text()[1]');
-		//Zotero.debug(updated);
-		
-		// populate the reference (the item)
-		for (i = 0; i < authors.length; i++){
-			item.creators.push(Zotero.Utilities.cleanAuthor(authors[i],"author"))
-		}
-		item.creators.push(Zotero.Utilities.cleanAuthor(deputy_editor,"editor"))
-		item.title = title;
-		item.date = updated;
-		item.url = "https://www.uptodate.com";
-		item.publicationTitle = "UpToDate";
-		item.place = "UpToDate, Waltham, MA"
 
-		// create link to the specific website through a url-snapshot
-		var linkurl = doc.location.href;
+		// --- URL CLEANING ---
+		// Removes "?search=..." query parameters
+		item.url = url.split('?')[0];
+
+		// --- TITLE EXTRACTION & TYPE CHECK ---
+		// Get the raw title from the DOM or Title tag
+		var title = ZU.xpathText(doc, '//*[@id="topicTitle"]');
+		if (!title) {
+			title = doc.title.replace(' - UpToDate', '').trim();
+		}
+
+		// Check if this is a "Drug information" page
+		// Check doc.title because it is consistent even if the DOM ID changes
+		if (doc.title.toLowerCase().includes("drug information")) {
+			item.publicationTitle = "UpToDate Lexidrug";
+			
+			// Optional: Remove ": Drug information" from the Title so it isn't duplicated
+			// Turns "Rituximab: Drug information" -> "Rituximab"
+			//title = title.replace(/:? Drug information/i, "").trim();
+		} else {
+			item.publicationTitle = "UpToDate";
+		}
+		
+		item.title = title;
+
+		// --- AUTHOR EXTRACTION ---
+		var contributorsDiv = doc.getElementById('topicContributors');
+		if (contributorsDiv) {
+			var children = contributorsDiv.children;
+			var currentType = 'author'; 
+			
+			for (var i = 0; i < children.length; i++) {
+				var child = children[i];
+				
+				if (child.tagName === 'DT') {
+					var label = child.textContent.toLowerCase();
+					if (label.includes('author')) currentType = 'author';
+					else if (label.includes('section editor')) currentType = 'editor'; 
+					else if (label.includes('deputy editor')) currentType = 'editor'; 
+					else currentType = null; 
+				}
+				
+				if (child.tagName === 'DD' && currentType) {
+					var name = child.textContent.trim();
+					// Clean credentials
+					name = name.replace(/,?\s*(MD|PhD|MBBS|DO|MPH|FACC|FACP|FRCP|MBChB|PharmD).*/g, ""); 
+					item.creators.push(Zotero.Utilities.cleanAuthor(name, currentType));
+				}
+			}
+		}
+
+		// --- DATE EXTRACTION ---
+		var lastUpdated = ZU.xpathText(doc, '//div[@class="litReviewSingleLine"][contains(., "This topic last updated")]');
+		if (lastUpdated) {
+			item.date = lastUpdated.replace(/.*:\s*/, '').replace(/\.$/, '');
+		}
+
+		item.publisher = "UpToDate, Inc.";
+		item.place = "Waltham, MA";
+		
 		item.attachments = [{
-			url: linkurl,
-			title: "UpToDate - " + title,
+			url: item.url,
+			title: "UpToDate - " + item.title,
 			mimeType: "text/html",
 			snapshot: false
 		}];
+
+		item.complete();
 	}
-	item.complete()
 }
 
-// Analysis of XPaths of contributer section for following uptodate article (2021-02-12):
-
-// https://www.uptodate.com/contents/mechanisms-of-atrial-fibrillation?source=mostViewed_widget
-
-//*[@id="topicContributors"]':
-//Authors:Brian Olshansky, MDRishi Arora, MD Section Editors:Bradley P Knight, MD, FACCHugh Calkins, MD Deputy Editor:Gordon M Saperia, MD
-
-//*[@id="topicContributors"]/dt[1]/text() -> Authors: 
-//*[@id="topicContributors"]/dd[1]/a -> Brian Olshansky, MD
-//*[@id="topicContributors"]/dd[2]/a -> Rishi Arora, MD
-//*[@id="topicContributors"]/dt[2]/text() -> Section Editors:
-//*[@id="topicContributors"]/dd[3]/a -> Bradley P Knight, MD, FACC
-//*[@id="topicContributors"]/dd[4]/a -> Hugh Calkins, MD
-//*[@id="topicContributors"]/dt[3]/text() -> Deputy Editor:
-//*[@id="topicContributors"]/dd[5]/a -> Gordon M Saperia, MD
+/** BEGIN TEST CASES **/
+var testCases = [
+]
+/** END TEST CASES **/
